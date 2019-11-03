6bd11d2a19d88c64e31c4fd370cddf8c363ed599
asterzwx <1702659@sit.singaporetech.edu.sg>
2019-11-03 12:24:29 +0800

    added http to localhost 4200 in application

--- src/main/java/hello/TokenController.java
+++ src/main/java/hello/TokenController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 import hello.model.UserAccount;
 
 @RestController
-@RequestMapping("/token")
+@RequestMapping("/rest/token")
 public class TokenController {
 
     private JwtGenerator jwtGenerator;
